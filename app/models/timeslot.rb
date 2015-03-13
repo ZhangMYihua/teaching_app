6e76b3cde5bc11d8b9b6b895ab17e72228857bbd
zhangmyihua <michaelzhang2@msn.com>
2015-03-13 19:02:43 -0400

    Added stuff to click on calendar

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,18 @@
 class Timeslot < ActiveRecord::Base
 	belongs_to :teaching
 	has_one :booking
+
+	def start
+		format_time start_time
+	end
+	
+	def end
+		format_time end_time
+	end
+
+
+	private
+	def format_time(t)
+		t.strftime("%Y-%m-%dT%H:%M:%S%z")
+	end
 end
