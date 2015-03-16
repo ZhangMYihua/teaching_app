a8ffaa436934524296544ff351f9f88797e6dfeb
Chris920 <cdtded1212@gmail.com>
2015-03-16 19:39:04 -0400

    added two versions of search form on the index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,31 +14,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+ #    logout
     reset_session
     redirect_to(:teachings, notice: 'Logged out!')
   end
 
-
-
-
-	# def new
-
- #  end
-
- #  def create
- #    @user = login(params[:email], params[:password])
-
- #    if @user
- #      session[:user_id] = @user.id
- #      redirect_to teachings_url, notice: "Logged in"
- #    else
- #      flash.now[:alert] = "Invalid email or password!"
- #      render :new
- #    end
- #  end
-
- #  def destroy
- #    logout
- #    redirect_to teachings_url, notice: "Logged out!"
- #  end
 end
