f568a0f57d8605fb61adbca082578c3638da0a65
Chris920 <cdtded1212@gmail.com>
2015-03-26 02:54:02 -0400

    I refactored the logic and improved the view in instructor#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,9 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update_attributes(user_params)
-  		redirect_to 
+  		redirect_to user_path(@user)
+  	else
+  		render "edit"
     end
   end
 
