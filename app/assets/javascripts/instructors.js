8776e83a336b1d2959a11e556db6ea68233dc04a
zhangmyihua <michaelzhang2@msn.com>
2015-03-29 15:14:50 -0400

    merging style changes

--- app/assets/javascripts/instructors.js
+++ app/assets/javascripts/instructors.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	$('#display-teachers').hide();		
+	$('#display-teachers').hide();
+  $('#back-button').hide();		
 });
+
+$('#back-button').click(function(){
+  $('#display-teachers').hide();
+  $('#back-button').hide(); 
+})
