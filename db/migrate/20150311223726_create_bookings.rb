d42da0bfd5f904d5504bd4c043557bebc41d0acf
Chris920 <cdtded1212@gmail.com>
2015-04-22 04:12:22 -0400

    retrying migrations

--- db/migrate/20150311223726_create_bookings.rb
+++ db/migrate/20150311223726_create_bookings.rb
@@ -3,8 +3,8 @@ class CreateBookings < ActiveRecord::Migration
     create_table :bookings do |t|
       t.text :description
       
-      t.string :student_id
-      t.string :timeslot_id
+      t.integer :student_id
+      t.integer :timeslot_id
 
       t.datetime :start_time
   		t.datetime :end_time 
